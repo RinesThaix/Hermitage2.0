94c11c73a1c06872ba9bacaed3325a0b9c3ea519
Artem Shalagin <artyom.shalagin@gmail.com>
2017-10-22 04:46:39 +0300

    i can do bfs!

---
+++ android/app/src/main/java/mem/kitek/android/map/PathFinder.java
@@ -0,0 +1,67 @@
+package mem.kitek.android.map;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+import mem.kitek.android.data.ApiData.HallInfo;
+import mem.kitek.android.data.Node.NodeInfo;
+
+public class PathFinder {
+    private List<NodeInfo> nodes;
+
+    public PathFinder(List<NodeInfo> nodes) {
+        this.nodes = nodes;
+    }
+
+    private List<NodeInfo> findPath(NodeInfo start, NodeInfo end) {
+        Queue<NodeInfo> queue = new ArrayDeque<>();
+        queue.add(start);
+        Set<NodeInfo> used = new HashSet<>();
+        Map<NodeInfo, NodeInfo> par = new HashMap<>();
+        while (!queue.isEmpty()) {
+            NodeInfo curr = queue.poll();
+            if (curr.getId() == end.getId()) {
+                break;
+            }
+            used.add(curr);
+            for (Integer nextInd : curr.getEdges()) {
+                NodeInfo next = nodes.get(nextInd);
+                if (!used.contains(next)) {
+                    queue.add(next);
+                    par.put(next, curr);
+                }
+            }
+        }
+        List<NodeInfo> path = new ArrayList<>();
+        NodeInfo curr = end;
+        while (curr != null && curr.getId() != start.getId()) { // actually never null I hope
+            curr = par.get(end);
+        }
+        return path;
+    }
+
+    public List<NodeInfo> findPath(List<NodeInfo> nodes, List<HallInfo> path) {
+        Map<HallInfo, List<NodeInfo>> hallToNode = new HashMap<>();
+        for (NodeInfo node : nodes) {
+            HallInfo hallInfo = node.getHall_info();
+            if (!hallToNode.containsKey(hallInfo)) {
+                hallToNode.put(hallInfo, new ArrayList<>());
+            }
+            hallToNode.get(hallInfo).add(node);
+        }
+
+        List<NodeInfo> nodePath = new ArrayList<>();
+        for (int i = 0; i < path.size() - 1; i++) {
+            NodeInfo start = hallToNode.get(path.get(i)).get(0); // TODO which one should I pick???
+            NodeInfo end = hallToNode.get(path.get(i + 1)).get(0);
+            nodePath.addAll(findPath(start, end));
+        }
+        return nodePath;
+    }
+}
